79cd7ab49ebc3f9559b39aad50c8a7d3f3544a71
Anthony Meleah <anthony.meleah@gmail.com>
2021-11-15 14:07:43 -0500

    more backend

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,11 +1,76 @@
-exports.getTransactions = (req,res,next) => {
-    res.send('GET transactions');
+const Transaction = require('../models/Transaction')
+
+exports.getTransactions = async (req,res,next) => {
+    try {
+        const transactions = await Transaction.find();
+
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
-exports.addTransaction = (req,res,next) => {
-    res.send('POST transaction');
+exports.addTransaction = async (req,res,next) => {
+
+    try {
+        const {text, amount} = req.body;
+
+        const transaction = await Transaction.create(req.body);
+
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        })
+
+    } catch (err) {
+        if(err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(val => val.message);
+
+            return res.status(400).json({
+                success: false,
+                errror: messages
+            });
+        } else {
+            return res.status(500).json({
+                succcess: false,
+                error: 'Server Error'
+            });
+        }
+
+    }
+    
 }
 
-exports.deleteTransaction = (req,res,next) => {
-    res.send('DELETE transaction');
+exports.deleteTransaction = async (req,res,next) => {
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+
+        if(!transaction) {
+            return res.status(404).json({
+                succcess: false,
+                error: 'No transaction found'
+            });
+        }
+
+        await transaction.remove();
+
+        return res.status(200).json({
+            success: true,
+            data: {}
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        })
+
+    }
 }
--- frontend/src/context/GlobalState.js
+++ frontend/src/context/GlobalState.js
@@ -1,5 +1,6 @@
 import React, {createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import axios from 'axios';
 
 const initialState = {
     transactions: []
@@ -9,6 +10,19 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    async function getTransactions() {
+        try {
+            const res = await axios.get('/api/v1/transactions');
+
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                payload: res.data.data
+            })
+        } catch (err) {
+
+        }
+    }
 
     function addTransaction(transaction) {
         dispatch({
